7851e250f8e7bad1e888672e44c98fad598a9324
Jim <james.schmitz@gmail.com>
2020-04-24 08:07:16 -0400

    pythonic name

--- py5jar/src/processing/core/Py5Methods.java
+++ py5jar/src/processing/core/Py5Methods.java
@@ -4,7 +4,7 @@ import processing.event.MouseEvent;
 
 public interface Py5Methods {
 
-  public void run_method(String methodName);
+  public void run_method(String method_name);
   
   public void mouse_wheel(MouseEvent event);
 }
