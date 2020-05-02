7dcf5208f294e6d79249c933023a93907f2fb84a
Jim <james.schmitz@gmail.com>
2020-05-02 14:46:29 -0400

    minor

--- experiments/class_test.py
+++ experiments/class_test.py
@@ -23,11 +23,11 @@ class Test(Sketch):
         self.fill(self.random(255), self.random(255), self.random(255), 50.0)
         self.rect(self.mouse_x, self.mouse_y, 40, 40)
 
-    # def mouse_moved(self):
-    #     # for i in range(100000):
-    #     #     self.foo += i * i * 17
-    #     self.foo = self.random(1)
-    #     print('frameRate', self.get_frame_rate())
+    def mouse_entered(self):
+        # for i in range(100000):
+        #     self.foo += i * i * 17
+        self.foo = self.random(1)
+        print('frameRate', self.get_frame_rate())
 
 
 test = Test()
