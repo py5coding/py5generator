67742974f4f591167bbfb14cc1461f0f8b1fad8a
Jim <james.schmitz@gmail.com>
2020-06-15 10:01:53 -0400

    parse block tags

--- py5_docs/src/Py5Doclet.java
+++ py5_docs/src/Py5Doclet.java
@@ -6,99 +6,134 @@ import java.util.Set;
 
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.ElementFilter;
-import javax.tools.Diagnostic.Kind;
+// import javax.tools.Diagnostic.Kind;
 
 import jdk.javadoc.doclet.Doclet;
 import jdk.javadoc.doclet.DocletEnvironment;
 import jdk.javadoc.doclet.Reporter;
 
 import com.sun.source.util.DocTrees;
+import com.sun.source.doctree.DocTree;
+// import com.sun.source.doctree.DocTree.Kind;
+import com.sun.source.doctree.ParamTree;
+import com.sun.source.doctree.SeeTree;
+import com.sun.source.doctree.UnknownBlockTagTree;
 import com.sun.source.doctree.DocCommentTree;
 
 public class Py5Doclet implements Doclet {
-  Reporter reporter;
-  @Override
-  public void init(Locale locale, Reporter reporter) {
-      reporter.print(Kind.NOTE, "Doclet using locale: " + locale);
-      this.reporter = reporter;
-  }
+    Reporter reporter;
 
-  public void printElement(DocTrees trees, Element e) {
-      DocCommentTree docCommentTree = trees.getDocCommentTree(e);
-      if (docCommentTree != null) {
-          System.out.println("Element (" + e.getKind() + ": "
-                  + e + ") has the following comments:");
-          System.out.println("Entire body: " + docCommentTree.getFullBody());
-          System.out.println("Block tags: " + docCommentTree.getBlockTags());
-      }
-  }
+    @Override
+    public void init(Locale locale, Reporter reporter) {
+        // reporter.print(Kind.NOTE, "Doclet using locale: " + locale);
+        this.reporter = reporter;
+    }
 
-  @Override
-  public boolean run(DocletEnvironment docEnv) {
-      DocTrees docTrees = docEnv.getDocTrees();
-      for (TypeElement t : ElementFilter.typesIn(docEnv.getIncludedElements())) {
-          System.out.println(t.getKind() + ":" + t);
-          for (Element e : t.getEnclosedElements()) {
-              printElement(docTrees, e);
-          }
-      }
-      return true;
-  }
+    public void printElement(DocTrees trees, Element e) {
+        ElementKind kind = e.getKind();
+        String name = e.getSimpleName().toString();
 
-  @Override
-  public String getName() {
-      return "Py5Doclet";
-  }
+        DocCommentTree docCommentTree = trees.getDocCommentTree(e);
+        if (docCommentTree != null) {
+            System.out.println("******************************************");
+            System.out.println(name + "(" + kind + ")");
 
-  @Override
-  public Set<? extends Option> getSupportedOptions() {
-      Option[] options = {
-          new Option() {
-              private final List<String> someOption = Arrays.asList(
-                      "-overviewfile",
-                      "--overview-file",
-                      "-o"
-              );
+            System.out.println("Entire body:");
+            for (DocTree tree : docCommentTree.getFullBody()) {
+                System.out.print(tree.toString().trim().replace("<br/>", "\n"));
+            }
 
-              @Override
-              public int getArgumentCount() {
-                  return 1;
-              }
+            System.out.println("\nBlock tags:");
+            for (DocTree tree : docCommentTree.getBlockTags()) {
+                switch (tree.getKind()) {
+                    case PARAM:
+                        ParamTree param = (ParamTree) tree;
+                        String pname = param.getName().toString();
+                        String desc = param.getDescription().toString();
+                        String istype = param.isTypeParameter() ? " (type)" : "";
+                        System.out.println(pname + istype + ": " + desc);
+                        break;
+                    case SEE:
+                        SeeTree see = (SeeTree) tree;
+                        String reference = see.getReference().toString();
+                        System.out.println("See Also: " + reference);
+                        break;
+                    case UNKNOWN_BLOCK_TAG:
+                        UnknownBlockTagTree unknown = (UnknownBlockTagTree) tree;
+                        String tagname = unknown.getTagName().toString();
+                        String content = unknown.getContent().toString();
+                        System.out.println(tagname + " " + content);
+                        break;
+                    default:
+                        System.out.println("???? DEFAULT ????");
+                        System.out.println(tree.getKind());
+                        System.out.println(tree.toString());
+                }
+            }
+        }
+    }
 
-              @Override
-              public String getDescription() {
-                  return "an option with aliases";
-              }
+    @Override
+    public boolean run(DocletEnvironment docEnv) {
+        DocTrees docTrees = docEnv.getDocTrees();
+        for (TypeElement t : ElementFilter.typesIn(docEnv.getIncludedElements())) {
+            System.out.println(t.getKind() + ":" + t);
+            for (Element e : t.getEnclosedElements()) {
+                printElement(docTrees, e);
+            }
+        }
+        return true;
+    }
 
-              @Override
-              public Option.Kind getKind() {
-                  return Option.Kind.STANDARD;
-              }
+    @Override
+    public String getName() {
+        return "Py5Doclet";
+    }
 
-              @Override
-              public List<String> getNames() {
-                  return someOption;
-              }
+    @Override
+    public Set<? extends Option> getSupportedOptions() {
+        Option[] options = { new Option() {
+            private final List<String> someOption = Arrays.asList("-overviewfile", "--overview-file", "-o");
 
-              @Override
-              public String getParameters() {
-                  return "file";
-              }
+            @Override
+            public int getArgumentCount() {
+                return 1;
+            }
 
-              @Override
-              public boolean process(String opt, List<String> arguments) {
-                  return true;
-              }
-          }
-      };
-      return new HashSet<Option>(Arrays.asList(options));
-  }
+            @Override
+            public String getDescription() {
+                return "an option with aliases";
+            }
 
-  @Override
-  public SourceVersion getSupportedSourceVersion() {
-      // support the latest release
-      return SourceVersion.latest();
-  }
+            @Override
+            public Option.Kind getKind() {
+                return Option.Kind.STANDARD;
+            }
+
+            @Override
+            public List<String> getNames() {
+                return someOption;
+            }
+
+            @Override
+            public String getParameters() {
+                return "file";
+            }
+
+            @Override
+            public boolean process(String opt, List<String> arguments) {
+                return true;
+            }
+        } };
+        return new HashSet<Option>(Arrays.asList(options));
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        // support the latest release
+        return SourceVersion.latest();
+    }
 }
