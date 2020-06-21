1b02b686299977cbd8c8735ace4b70d198d838eb
Jim <james.schmitz@gmail.com>
2020-06-21 11:37:13 -0400

    better conversion functions

--- experiments/pimage_test.py
+++ experiments/pimage_test.py
@@ -4,9 +4,9 @@ import pandas as pd
 import matplotlib.style as mplstyle
 import matplotlib
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-matplotlib.use('agg')
-# mplstyle.use('fast')
-mplstyle.use(['ggplot', 'fast'])
+matplotlib.use('svg')
+mplstyle.use('fast')
+# mplstyle.use(['ggplot', 'fast'])
 
 from PIL import Image
 
@@ -20,36 +20,37 @@ def settings():
 def draw():
     py5.image_mode(py5.CENTER)
     py5.background(0)
-    # py5.fill(255)
-    # py5.rect(0, 0, py5.width / 2, py5.height / 2)
-
-    # img = np.zeros((50, 20, 4), dtype=np.uint8)
-    # img[:, :, 3] = 255
-    # img[:30, :, 2] = 255
-    # img[10:40, :, 1] = 255
-    # img[20:, :, 0] = 255
 
     def convert_pillow_image_to_pimage(img):
-        X = convert_rgba_to_argb(np.asarray(img.convert("RGBA")))
-        height, width, _ = X.shape
-        return py5.get_py5applet().convertBytesToPImage(X.tobytes(), width, height, pass_by_reference=False)
-
-    def convert_rgba_to_argb(X):
-        a, b = np.split(X, axis=2, indices_or_sections=np.array([3]))
-        return np.block([b, a])
+        return convert_numpy_to_pimage(np.asarray(img), bands=img.mode)
 
     def convert_figure_to_pimage(figure):
         canvas = FigureCanvasAgg(figure)
         canvas.draw()
-        X = convert_rgba_to_argb(np.asarray(canvas.buffer_rgba()))
-        height, width, _ = X.shape
-        return py5.get_py5applet().convertBytesToPImage(X.tobytes(), width, height, pass_by_reference=False)
+        return convert_numpy_to_pimage(np.asarray(canvas.buffer_rgba()), bands="RGBA")
 
-    # img = Image.open('../py5projects/drawingtutor/data/jpg/01003.jpg').resize((500, 500))
-    # py5.image(convert_pillow_image_to_pimage(img), py5.width / 2, py5.height / 2)
+    def convert_numpy_to_pimage(arr, bands="ARGB"):
+        height, width, _ = arr.shape
+        if bands == "RGBA":
+            a, b = np.split(arr, axis=2, indices_or_sections=np.array([3]))
+            arr = np.block([b, a])
+        elif bands == "RGB":
+            arr = np.block([np.full((height, width, 1), 255, dtype=np.uint8), arr])
 
-    figure = pd._testing.makeTimeDataFrame().plot().figure
-    py5.image(convert_figure_to_pimage(figure), py5.width / 2, py5.height / 2)
+        return py5.get_py5applet().convertBytesToPImage(arr.tobytes(), width, height, pass_by_reference=False)
+
+    img = Image.open('../py5projects/drawingtutor/data/jpg/01003.jpg').resize((500, 500))
+    py5.image(convert_pillow_image_to_pimage(img), py5.width / 2, py5.height / 2)
+
+    # figure = pd._testing.makeTimeDataFrame().plot().figure
+    # py5.image(convert_figure_to_pimage(figure), py5.width / 2, py5.height / 2)
+
+    # img = np.zeros((500, 500, 4), dtype=np.uint8)
+    # img[:, :, 0] = 255
+    # img[:200, :, 1] = 200
+    # img[100:400, :, 2] = 100
+    # img[300:, :, 3] = 150
+    # py5.image(convert_numpy_to_pimage(img), py5.width / 2, py5.height / 2)
 
     py5.no_loop()
 
