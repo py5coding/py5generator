4518aab027c6d4421f11a50703923aef362fba8c
Jim <james.schmitz@gmail.com>
2020-06-24 22:15:37 -0400

    get conversions working

--- experiments/pimage_test.py
+++ experiments/pimage_test.py
@@ -3,13 +3,7 @@ import pandas as pd
 
 import matplotlib.style as mplstyle
 import matplotlib
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-# from matplotlib.backends.backend_svg import FigureCanvasSVG
-from matplotlib.backends.backend_cairo import FigureCanvasCairo
-import cairocffi
-import cairosvg
-# from cairo import SVGSurface, SVG_VERSION_1_1
-from PIL import Image
+import cairo
 import py5
 
 mplstyle.use(['ggplot', 'fast'])
@@ -26,9 +20,9 @@ def draw():
     py5.shape_mode(py5.CENTER)
     py5.background(128)
 
-    img = py5.load_image('/tmp/example.svg')
+    # img = py5.load_image('/tmp/example.svg')
     # img = py5.load_image('/mnt/readynas_data/DataBackup/ffhq_dataset/jpg/01003.jpg').resize((500, 500))
-    py5.image(img, py5.width / 2, py5.height / 2)
+    # py5.image(img, py5.width / 2, py5.height / 2)
 
     # figure = pd._testing.makeTimeDataFrame().plot().figure
     # py5.image(figure, py5.width / 2, py5.height / 2)
@@ -40,8 +34,24 @@ def draw():
     # img[300:, :, 3] = 150
     # py5.image((img, 'ARGB'), py5.width / 2, py5.height / 2)
 
-    # figure = pd._testing.makeTimeDataFrame().plot().figure
-    # py5.shape(figure, py5.width / 2, py5.height / 2)
+    with cairo.RecordingSurface(cairo.Content.COLOR_ALPHA, cairo.Rectangle(0, 0, 200, 200)) as surface:
+    # with cairo.SVGSurface(None, 200, 200) as surface:
+        context = cairo.Context(surface)
+        x, y, x1, y1 = 0.1, 0.5, 0.4, 0.9
+        x2, y2, x3, y3 = 0.6, 0.1, 0.9, 0.5
+        context.scale(200, 200)
+        context.set_line_width(0.04)
+        context.move_to(x, y)
+        context.curve_to(x1, y1, x2, y2, x3, y3)
+        context.stroke()
+        context.set_source_rgba(1, 0.2, 0.2, 0.6)
+        context.set_line_width(0.02)
+        context.move_to(x, y)
+        context.line_to(x1, y1)
+        context.move_to(x2, y2)
+        context.line_to(x3, y3)
+        context.stroke()
+        py5.image(surface, py5.width / 2, py5.height / 2)
 
     py5.no_loop()
 
--- py5_resources/py5_module/py5/conversions.py
+++ py5_resources/py5_module/py5/conversions.py
@@ -1,4 +1,3 @@
-import io
 import tempfile
 
 import numpy as np
@@ -7,27 +6,14 @@ import cairocffi
 import cairo
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib.backends.backend_cairo import FigureCanvasCairo
 
 
 class Converter:
 
     pimage_functions = []
-    pshape_functions = []
 
     def __init__(self, py5applet):
         self._py5applet = py5applet
-
-    def _convert_numpy_to_pimage(self, arr, bands):
-        height, width, _ = arr.shape
-        bands = bands.upper()
-        if bands == 'RGBA':
-            a, b = np.split(arr, axis=2, indices_or_sections=np.array([3]))
-            arr = np.block([b, a])
-        elif bands == 'RGB':
-            arr = np.block([np.full((height, width, 1), 255, dtype=np.uint8), arr])
-
-        return self._py5applet.convertBytesToPImage(arr.tobytes(), width, height, pass_by_reference=False)
 
     def _convert_cairo_to_pshape(self, canvas):
         with tempfile.NamedTemporaryFile(suffix='.svg') as temp:
@@ -35,66 +21,95 @@ class Converter:
             return self._py5applet.loadShape(temp.name)
 
     def to_pimage(self, obj):
-        orig_obj = obj
-        for obj_type, function in Converter.pimage_functions:
-            if isinstance(obj, obj_type):
-                return self._convert_numpy_to_pimage(*function(obj))
+        for converter in Converter.pimage_functions:
+            if converter.precondition(obj):
+                obj = converter.convert(obj)
+                break
+        else:
+            raise RuntimeError(f'Py5 Converter does not know how to convert {str(obj)}')
 
-        if isinstance(obj, tuple):
-            if (len(obj) == 2
+        if isinstance(obj, np.ndarray):
+            height, width, _ = obj.shape
+            return self._py5applet.convertBytesToPImage(obj.tobytes(), width, height, pass_by_reference=False)
+        if isinstance(obj, tempfile._TemporaryFileWrapper):
+            pimage = self._py5applet.loadImage(obj.name)
+            obj.close()
+            return pimage
+
+        raise RuntimeError(f'Error in Py5 Converter for {str(obj)}')
+
+    @classmethod
+    def register_pimage_conversion(cls, converter_class):
+        cls.pimage_functions.append(converter_class)
+
+
+class NDArrayStrTuple:
+
+    @classmethod
+    def precondition(cls, obj):
+        return (isinstance(obj, tuple)
+                and len(obj) == 2
                 and isinstance(obj[0], np.ndarray)
                 and obj[0].dtype == np.uint8
                 and obj[0].ndim == 3
                 and isinstance(obj[1], str)
                 and obj[1].upper() in ['RGBA', 'ARGB', 'RGB']
-                    and obj[0].shape[2] == len(obj[1])):
-                return self._convert_numpy_to_pimage(*obj)
-            else:
-                raise RuntimeError("Tuple parameter must be a numpy array and a string")
-        else:
-            raise RuntimeError(f'Py5 Converter does not know how to convert an object of type {str(type(orig_obj))}')
-
-    def to_pshape(self, obj):
-        if type(obj) in Converter.pshape_functions:
-            return self._convert_cairo_to_pshape(Converter.pshape_functions[type(obj)](obj))
-        else:
-            raise RuntimeError(f'Py5 Converter does not know how to convert an object of type {str(type(obj))}')
+                and obj[0].shape[2] == len(obj[1]))
 
     @classmethod
-    def register_pimage_conversion(cls, obj_type, function):
-        cls.pimage_functions.append((obj_type, function))
+    def convert(cls, obj):
+        arr, bands = obj
+        bands = bands.upper()
+        if bands == 'RGBA':
+            a, b = np.split(arr, axis=2, indices_or_sections=np.array([3]))
+            arr = np.block([b, a])
+        elif bands == 'RGB':
+            height, width, _ = arr.shape
+            arr = np.block([np.full((height, width, 1), 255, dtype=np.uint8), arr])
+
+        return arr
+
+
+class FigureToNDArray:
 
     @classmethod
-    def register_pshape_conversion(cls, obj_type, function):
-        cls.pshape_functions.append((obj_type, function))
+    def precondition(cls, obj):
+        return isinstance(obj, Figure)
+
+    @classmethod
+    def convert(cls, figure):
+        canvas = FigureCanvasAgg(figure)
+        canvas.draw()
+        return NDArrayStrTuple.convert((np.asarray(canvas.buffer_rgba()), 'RGBA'))
 
 
-def convert_figure_to_ndarray(figure):
-    canvas = FigureCanvasAgg(figure)
-    canvas.draw()
-    return np.asarray(canvas.buffer_rgba()), 'RGBA'
+class PillowImageToNDArray:
+
+    @classmethod
+    def precondition(cls, obj):
+        return isinstance(obj, Image.Image)
+
+    @classmethod
+    def convert(cls, img):
+        if img.mode not in ['RGB', 'RGBA']:
+            img = img.convert(mode='RGB')
+        return NDArrayStrTuple.convert((np.asarray(img), img.mode))
 
 
-def convert_pillow_image_to_ndarray(img):
-    return np.asarray(img), img.mode
+class CairoSurfaceToTempfile:
+
+    @classmethod
+    def precondition(cls, obj):
+        return isinstance(obj, (cairocffi.Surface, cairo.Surface))
+
+    @classmethod
+    def convert(cls, surface):
+        temp_png = tempfile.NamedTemporaryFile(suffix='.png')
+        surface.write_to_png(temp_png.name)
+        return temp_png
 
 
-def convert_cairo_surface_to_ndarray(surface):
-    buffer = io.BytesIO()
-    surface.write_to_png(buffer)
-    img = Image.open(buffer)
-    return convert_pillow_image_to_ndarray(img)
-
-
-def convert_figure_to_canvas(figure):
-    canvas = FigureCanvasCairo(figure)
-    canvas.draw()
-    return canvas
-
-
-Converter.register_pimage_conversion(Figure, convert_figure_to_ndarray)
-Converter.register_pimage_conversion(Image.Image, convert_pillow_image_to_ndarray)
-Converter.register_pimage_conversion(cairocffi.Surface, convert_cairo_surface_to_ndarray)
-Converter.register_pimage_conversion(cairo.Surface, convert_cairo_surface_to_ndarray)
-
-Converter.register_pshape_conversion(Figure, convert_figure_to_canvas)
+Converter.register_pimage_conversion(NDArrayStrTuple)
+Converter.register_pimage_conversion(FigureToNDArray)
+Converter.register_pimage_conversion(PillowImageToNDArray)
+Converter.register_pimage_conversion(CairoSurfaceToTempfile)
