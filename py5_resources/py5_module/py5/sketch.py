36b1535021af20fdbba25f9b1c7511a84c569f00
Jim <james.schmitz@gmail.com>
2020-06-25 18:34:21 -0400

    fix caching and add create_image

--- py5_resources/py5_module/py5/sketch.py
+++ py5_resources/py5_module/py5/sketch.py
@@ -91,19 +91,38 @@ class Sketch:
 
     # *** PImage replacement methods ***
 
-    # TODO: add type hint functions
+    @overload
+    def image(self, img: Any, a: float, b: float, cache: bool) -> None:
+        """$class_image"""
+        pass
+
+    @overload
+    def image(self, img: Any, a: float, b: float, c: float, d: float, cache: bool) -> None:
+        """$class_image"""
+        pass
+
     def image(self, *args, cache: bool = False) -> None:
         """$class_image"""
-        if cache and id(args[0]) in self._pimage_cache:
-            img = self._pimage_cache[id(args[0])]
+        arg0_id = id(args[0])
+        if cache and arg0_id in self._pimage_cache:
+            pimage = self._pimage_cache[arg0_id]
         else:
-            img = self._converter.to_pimage(args[0])
-            if cache:
-                self._pimage_cache[id(args[0])] = img
+            pimage = self._converter.to_pimage(args[0])
 
-        self._py5applet.image(img, *args[1:])
+        if cache:
+            self._pimage_cache[arg0_id] = pimage
 
-    def load_image(self, filename) -> Image.Image:
+        self._py5applet.image(pimage, *args[1:])
+
+    # TODO: what about alpha mask images?
+    # TODO: are there other PImage functions I should be paying attention to?
+    # TODO: does caching actually work?
+
+    def create_image(self, mode: str, width: int, height: int, color: Any) -> Image.Image:
+        """$class_create_image"""
+        return Image.new(mode, (width, height), color)
+
+    def load_image(self, filename: Union[str, Path]) -> Image.Image:
         """$class_load_image"""
         filename = Path(filename)
         if filename.suffix.lower() == '.svg':
@@ -111,6 +130,18 @@ class Sketch:
                 return Image.open(io.BytesIO(cairosvg.svg2png(file_obj=f)))
         else:
             return Image.open(filename)
+
+    def texture(self, image: Any, cache: bool = False) -> None:
+        """$class_texture"""
+        image_id = id(image)
+        if cache and image_id in self._pimage_cache:
+            pimage = self._pimage_cache[image_id]
+        else:
+            pimage = self._converter.to_pimage(image)
+        if cache:
+            self._pimage_cache[image_id] = pimage
+
+        self._py5applet.texture(pimage)
 
     # *** Math (numpy) methods ***
 
