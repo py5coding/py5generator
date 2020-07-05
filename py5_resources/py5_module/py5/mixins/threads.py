bcfc0d50e67e89779aa407d6b1ae169e80b41ff6
Jim <james.schmitz@gmail.com>
2020-07-05 12:29:32 -0400

    remove dead threads and combine repeating and timed repeating threads into one class

--- py5_resources/py5_module/py5/mixins/threads.py
+++ py5_resources/py5_module/py5/mixins/threads.py
@@ -28,33 +28,15 @@ class Py5Thread:
 
 class Py5Repeater(Py5Thread):
 
-    def __init__(self, sketch, f, args, kwargs):
-        super().__init__(sketch, f, args, kwargs)
-        self.repeat = True
-
-    def stop(self):
-        super().stop()
-        self.repeat = False
-
-    def __call__(self):
-        try:
-            while self.repeat:
-                self.f(*self.args, **self.kwargs)
-        except Exception:
-            self.stop()
-            methods.handle_exception(*sys.exc_info())
-            self.sketch._terminate_sketch()
-
-
-class Py5TimeDelayRepeater(Py5Repeater):
-
     def __init__(self, sketch, f, delay, args, kwargs):
         super().__init__(sketch, f, args, kwargs)
+        self.repeat = True
         self.delay = delay
         self.e = threading.Event()
 
     def stop(self):
         super().stop()
+        self.repeat = False
         self.e.set()
 
     def __call__(self):
@@ -94,28 +76,31 @@ class ThreadsMixin:
 
         t = threading.Thread(target=py5thread, name=name)
         t.start()
+        self._py5threads[t.name] = (t, py5thread)
 
-        self._py5threads[name] = (t, py5thread)
+        return t.name
 
-    def launch_thread(self, f: Callable, name: str, args: Tuple = None, kwargs: Dict = None) -> None:
+    def launch_thread(self, f: Callable, name: str = None,
+                      args: Tuple = None, kwargs: Dict = None) -> str:
         """$class_launch_thread"""
         args, kwargs = self._check_param_types(args, kwargs)
-        self._launch_py5thread(name, Py5Thread(self, f, args, kwargs))
+        return self._launch_py5thread(name, Py5Thread(self, f, args, kwargs))
 
-    def launch_repeating_thread(self, f: Callable, name: str,
-                                args: Tuple = None, kwargs: Dict = None) -> None:
+    def launch_repeating_thread(self, f: Callable, name: str = None, time_delay: float = 0,
+                                args: Tuple = None, kwargs: Dict = None) -> str:
         """$class_launch_repeating_thread"""
         args, kwargs = self._check_param_types(args, kwargs)
-        self._launch_py5thread(name, Py5Repeater(self, f, args, kwargs))
+        return self._launch_py5thread(name, Py5Repeater(self, f, time_delay, args, kwargs))
 
-    def launch_repeating_time_thread(self, f: Callable, name: str, time_delay: float,
-                                     args: Tuple = None, kwargs: Dict = None) -> None:
-        """$class_launch_repeating_time_thread"""
-        args, kwargs = self._check_param_types(args, kwargs)
-        self._launch_py5thread(name, Py5TimeDelayRepeater(self, f, time_delay, args, kwargs))
+    def _remove_dead_threads(self):
+        thread_names = list(self._py5threads.keys())
+        for t_name in thread_names:
+            if not self._py5threads[t_name][0].is_alive():
+                del self._py5threads[t_name]
 
     def has_thread(self, name: str) -> None:
         """$class_has_thread"""
+        self._remove_dead_threads()
         return name in self._py5threads
 
     def stop_thread(self, name: str, wait: bool = False) -> None:
@@ -132,12 +117,13 @@ class ThreadsMixin:
         current_thread_name = threading.current_thread().name
         for name in self.list_threads():
             if name == current_thread_name:
-                # can't join thread with itself :(
+                # don't try to join a thread with itself
                 continue
             self.stop_thread(name, wait=wait)
 
     def list_threads(self) -> None:
         """$class_list_threads"""
+        self._remove_dead_threads()
         return list(self._py5threads.keys())
 
     def _shutdown(self):
