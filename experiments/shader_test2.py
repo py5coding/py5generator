eec918fee2f653d1315a76ff02af9c6b77ba32d3
Jim <james.schmitz@gmail.com>
2020-07-10 17:53:49 -0400

    better shader example

---
+++ experiments/shader_test2.py
@@ -0,0 +1,32 @@
+import py5
+
+tile_texture = None
+tile_shader = None
+
+
+def settings():
+    py5.size(640, 480, py5.P2D)
+
+
+def setup():
+    global tile_texture
+    py5.texture_wrap(py5.REPEAT)
+    tile_texture = py5.load_image("data/penrose.jpg")
+    load_tile_shader()
+
+
+def load_tile_shader():
+    global tile_texture
+    global tile_shader
+    tile_shader = py5.load_shader("scroller.glsl")
+    tile_shader.set("resolution", py5.width, py5.height)
+    tile_shader.set("tileImage", tile_texture)
+
+
+def draw():
+    tile_shader.set("time", py5.millis() / 1000.0)
+    py5.shader(tile_shader)
+    py5.rect(0, 0, py5.width, py5.height)
+
+
+py5.run_sketch(block=True)
