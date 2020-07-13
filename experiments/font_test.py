719114a13fa759080ba0fceca7a41cbc3abe9ad9
Jim <james.schmitz@gmail.com>
2020-07-13 09:57:12 -0400

    minor repairs

--- experiments/font_test.py
+++ experiments/font_test.py
@@ -4,9 +4,6 @@ from py5 import Sketch
 
 class Test(Sketch):
 
-    # def __init__(self):
-    #     super().__init__()
-
     def settings(self):
         self.size(600, 500, py5.P2D)
 
@@ -15,13 +12,13 @@ class Test(Sketch):
         self.font = self.load_font("Ubuntu-32.vlw")
         self.text_align(py5.CENTER, py5.CENTER)
         self.fill(255)
+        self.no_loop()
 
     def draw(self):
         self.background(0)
         self.text_font(self.font)
         letters = 'test message'
         self.text(letters, self.width / 2, self.height / 2)
-        self.no_loop()
 
 
 test = Test()
