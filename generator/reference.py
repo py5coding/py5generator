bc209c5b402c3840b53b6fcc69ba91bc52cd2374
Jim <james.schmitz@gmail.com>
2020-07-14 17:55:58 -0400

    clean up extra dir names

--- generator/reference.py
+++ generator/reference.py
@@ -35,6 +35,6 @@ PCONSTANT_OVERRIDES = {
 
 EXTRA_DIR_NAMES = {
     'run_sketch', 'get_py5applet', 'reset_py5', 'exit_sketch',
-    'autoclass', 'Py5Methods', '_Py5Applet', '_py5sketch', '_py5sketch_used',
+    'autoclass', 'Py5Exception', 'Sketch',
     'prune_tracebacks', 'set_stackprinter_style', 'create_font_file'
 }
