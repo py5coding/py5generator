8d128586b1f30b54ec490846b33d415e7076b321
Jim <james.schmitz@gmail.com>
2020-07-15 10:11:32 -0400

    fix create font tool

--- py5_resources/py5_module/py5/create_font_tool.py
+++ py5_resources/py5_module/py5/create_font_tool.py
@@ -22,15 +22,15 @@ class CreateFontTool(Sketch):
         self.fill(0)
 
         self.text_font(font)
-        characters = self.characters or ''.join(font._pfont.CHARSET)
+        characters = self.characters or ''.join(font._instance.CHARSET)
         self.text(characters, self.width / 2, self.height / 2)
 
         os = self.get_py5applet().createOutput(str(self.filename))
-        font._pfont.save(os)
+        font._instance.save(os)
         os.close()
 
         self.fill(255)
-        msg = str(font._pfont.getGlyphCount()) + ' glyphs written to ' + self.filename
+        msg = str(font._instance.getGlyphCount()) + ' glyphs written to ' + self.filename
         self.translate(self.width / 2, self.height / 2)
         self.scale(0.95 * self.width / self.text_width(msg))
         self.text(msg, 0, 0)
