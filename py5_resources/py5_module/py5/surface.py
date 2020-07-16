a8b46e4755fc9531f12be9ac5fb04b42fa10b387
Jim <james.schmitz@gmail.com>
2020-07-16 11:54:51 -0400

    fix image cache

--- py5_resources/py5_module/py5/surface.py
+++ py5_resources/py5_module/py5/surface.py
@@ -14,14 +14,15 @@ py5surface_class_members_code = None  # DELETE
 def _return_py5surface(f):
     @functools.wraps(f)
     def decorated(self_, *args):
-        return Py5Surface(f(self_, *args))
+        return Py5Surface(f(self_, *args), getattr(self_, '_pimage_cache', None))
     return decorated
 
 
 class Py5Surface:
 
-    def __init__(self, psurface):
+    def __init__(self, psurface, pimage_cache):
         self._instance = psurface
+        self._pimage_cache = pimage_cache
 
 
 {py5surface_class_members_code}
