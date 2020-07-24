0fdaaf2279fdaab123f6e616f5471e7b111333a5
Jim <james.schmitz@gmail.com>
2020-07-24 11:57:47 -0400

    improve example and get it working again

--- experiments/pde_test.py
+++ experiments/pde_test.py
@@ -1,12 +1,7 @@
 # flake8: noqa
-import test_import as ti
-
-
-started = False
-
 
 def settings():
-    size(500, 500, JAVA2D)
+    size(500, 500, P2D)
 
 
 def setup():
@@ -17,24 +12,17 @@ def setup():
 def draw():
     if is_key_pressed():
         print('frameRate', get_frame_rate())
-    if started:
-        fill(random(255), random(255), random(255), 50.0)
-        rect(mouse_x, mouse_y, 40, 40)
+
+    fill(random(255), random(255), random(255), 50.0)
+    rect(mouse_x, mouse_y, 40, 40)
 
     if frame_count == 300:
         save_frame('/tmp/frame_###.png')
-        cause_error(ti.test_function(10, 30))
 
 
 def mouse_entered():
-    global started
-    started = True
     print('mouse entered')
 
 
 def mouse_exited():
     print('mouse exited')
-
-
-def cause_error(x):
-    print(x / 0)
