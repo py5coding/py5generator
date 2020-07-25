d9ec82df50633cb00519b57b5cfcb24e87d7e374
Jim <james.schmitz@gmail.com>
2020-07-25 11:53:09 -0400

    pfont conversion

--- py5_resources/py5_module/py5/font.py
+++ py5_resources/py5_module/py5/font.py
@@ -19,16 +19,6 @@ def _return_py5font(f):
     return decorated
 
 
-def _py5font_param(f):
-    @functools.wraps(f)
-    def decorated(self_, *args):
-        if isinstance(args[0], Py5Font):
-            args = (args[0]._instance, *args[1:])
-        return f(self_, *args)
-
-    return decorated
-
-
 class Py5Font:
 
     _cls = _PFont
--- py5_resources/py5_module/py5/java_conversion.py
+++ py5_resources/py5_module/py5/java_conversion.py
@@ -1,8 +1,13 @@
 from jpype import _jcustomizer
 
-from .sketch import Py5Image
+from .sketch import Py5Image, Py5Font
 
 
 @_jcustomizer.JConversion("processing.core.PImage", instanceof=Py5Image)
-def _py5image_convert(jcls, obj):
+def _pimage_convert(jcls, obj):
+    return obj._instance
+
+
+@_jcustomizer.JConversion("processing.core.PFont", instanceof=Py5Font)
+def _pfont_convert(jcls, obj):
     return obj._instance
