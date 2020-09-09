93e8768d31f52e6e355e83aad3e048a0ec88921d
Jim <james.schmitz@gmail.com>
2020-09-09 10:13:13 -0400

    code improvements and handle examples with no images

--- experiments/doc_format_test.py
+++ experiments/doc_format_test.py
@@ -6,9 +6,9 @@ import xmltodict
 
 test_text = """## meta
 name = noise()
-category = Math
-subcategory = Random
-type = method
+category =Math
+subcategory= Random
+type=method
 
 ## description
 this is awesome
@@ -21,11 +21,17 @@ image = foo.jpg
 foo = 10 # asdf
 py5.size(10, 20)
 py5.rect(1, 2, 3, 4)
+
+## example
+
+bar = 10 # asdf
+py5.size(100, 200)
+py5.rect(1, 2, 3, 4)
 """
 
 DOC_REGEX = re.compile(r'(?<=## )(\w+)\s(.*?)(?=##|$)', re.DOTALL)
-META_REGEX = re.compile(r'(\w*) = (.*)')
-CODE_REGEX = re.compile(r'image = ([\w\d\.]+)\s+(.*)', re.DOTALL)
+META_REGEX = re.compile(r'(\w*)\s?=\s?(.*)')
+CODE_REGEX = re.compile(r'image\s?=\s?([\w\d\.]+)\s+(.*)', re.DOTALL)
 
 
 class Documentation:
@@ -33,33 +39,32 @@ class Documentation:
     def __init__(self, filename):
         if not isinstance(filename, Path):
             filename = Path(filename)
-        if filename.suffix == '.txt':
-            self.meta, self.examples, self.description = self._from_txt_file(filename)
-        elif filename.suffix == '.xml':
-            self.meta, self.examples, self.description = self._from_xml_file(filename)
-
-    def _from_xml_file(self, filename):
         with open(filename, 'r') as f:
-            content = xmltodict.parse(f.read())['root']
+            content = f.read()
+        if filename.suffix == '.txt':
+            self.meta, self.examples, self.description = self._from_txt(content)
+        elif filename.suffix == '.xml':
+            self.meta, self.examples, self.description = self._from_xml(content)
+
+    def _from_xml(self, content):
+        xml = xmltodict.parse(content)['root']
         meta = dict()
         examples = []
         description = ''
-        for key in content.keys():
+        for key in xml.keys():
             if key == 'description':
-                description = content['description']
+                description = xml['description']
             elif key == 'example':
-                example = content['example']
+                example = xml['example']
                 if isinstance(example, list):
                     examples = [(x.get('image'), x['code']) for x in example]
                 else:
                     examples = [(example.get('image'), example['code'])]
             else:
-                meta[key] = content[key]
+                meta[key] = xml[key]
         return meta, examples, description
 
-    def _from_txt_file(self, filename):
-        with open(filename, 'r') as f:
-            text = f.read()
+    def _from_txt(self, text):
         meta = dict()
         examples = []
         description = ''
@@ -67,8 +72,10 @@ class Documentation:
             if kind == 'meta':
                 meta = dict(META_REGEX.findall(content))
             elif kind == 'example':
-                # TODO: this is wrong, there might not be an image
-                examples.append(CODE_REGEX.match(content.strip()).groups())
+                if m := CODE_REGEX.match(content.strip()):
+                    examples.append(m.groups())
+                else:
+                    examples.append((None, content.strip()))
             elif kind == 'description':
                 description = content.strip()
         return meta, examples, description
@@ -78,6 +85,11 @@ filename1 = 'py5_docs/Reference/api_en/Sketch_tint.xml'
 filename2 = 'py5_docs/Reference/api_en/Sketch_bezier_detail.xml'
 doc1 = Documentation(filename1)
 doc2 = Documentation(filename2)
+
+filename3 = '/tmp/test_text.txt'
+with open(filename3, 'w') as f:
+    f.write(test_text)
+doc3 = Documentation(filename3)
 
 for filename in Path('py5_docs/Reference/api_en/').glob('*.xml'):
     Documentation(filename)
