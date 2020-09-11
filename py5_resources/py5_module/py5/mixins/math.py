3da6f89ab9ee3fab409ba2629ceb660700161e51
Jim <james.schmitz@gmail.com>
2020-09-11 12:17:03 -0400

    add docstring keys to math mixin

--- py5_resources/py5_module/py5/mixins/math.py
+++ py5_resources/py5_module/py5/mixins/math.py
@@ -10,46 +10,57 @@ class MathMixin:
 
     @classmethod
     def sin(cls, angle: float) -> float:
+        """$class_Sketch_sin"""
         return np.sin(angle)
 
     @classmethod
     def cos(cls, angle: float) -> float:
+        """$class_Sketch_cos"""
         return np.cos(angle)
 
     @classmethod
     def tan(cls, angle: float) -> float:
+        """$class_Sketch_tan"""
         return np.tan(angle)
 
     @classmethod
     def asin(cls, value: float) -> float:
+        """$class_Sketch_asin"""
         return np.arcsin(value)
 
     @classmethod
     def acos(cls, value: float) -> float:
+        """$class_Sketch_acos"""
         return np.arccos(value)
 
     @classmethod
     def atan(cls, value: float) -> float:
+        """$class_Sketch_atan"""
         return np.arctan(value)
 
     @classmethod
     def atan2(cls, y: float, x: float) -> float:
+        """$class_Sketch_atan2"""
         return np.arctan2(y, x)
 
     @classmethod
     def degrees(cls, radians: float) -> float:
+        """$class_Sketch_degrees"""
         return np.degrees(radians)
 
     @classmethod
     def radians(cls, degrees: float) -> float:
+        """$class_Sketch_radians"""
         return np.radians(degrees)
 
     @classmethod
     def constrain(cls, amt: float, low: float, high: float) -> float:
+        """$class_Sketch_constrain"""
         return np.where(amt < low, low, np.where(amt > high, high, amt))
 
     @classmethod
     def dist(cls, *args) -> float:
+        """$class_Sketch_dist"""
         p1 = args[:(len(args) // 2)]
         p2 = args[(len(args) // 2):]
         assert len(p1) == len(p2)
@@ -57,16 +68,20 @@ class MathMixin:
 
     @classmethod
     def lerp(cls, start: float, stop: float, amt: float) -> float:
+        """$class_Sketch_lerp"""
         return amt * (stop - start) + start
 
     @classmethod
     def mag(cls, *args) -> float:
+        """$class_Sketch_mag"""
         return sum([x * x for x in args])**0.5
 
     @classmethod
     def norm(cls, value: float, start: float, stop: float) -> float:
+        """$class_Sketch_norm"""
         return (value - start) / (stop - start)
 
     @classmethod
     def sq(cls, n: float) -> float:
+        """$class_Sketch_sq"""
         return n * n
