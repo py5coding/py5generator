5a3173f67e2d0f4a40c8b9b05a92dac2457340e6
Jim <james.schmitz@gmail.com>
2020-11-03 17:26:13 -0500

    handle docs with no examples, no parameters, and non-methods

--- py5_docs/scripts/create_reference_docs.py
+++ py5_docs/scripts/create_reference_docs.py
@@ -6,9 +6,7 @@ import pandas as pd
 
 from generator.docfiles import Documentation
 
-# TODOs:
-# Need different template for non-functions
-# Docs with no examples or parameters should skip those sections
+# TODO: Need method/attribute index pages for Sketch and other classes
 
 
 # PY5_API_EN = Path('py5_docs/Reference/api_en/')
@@ -27,53 +25,59 @@ DOC_TEMPLATE = """.. title: {0}
 .. type: text
 
 {3}
-
-Examples
-========
-
 {4}
-
 Description
 ===========
 
 {5}
-
-Syntax
-======
-
-.. code:: python
-
 {6}
-
-Parameters
-==========
-
 {7}
-
 Updated on {8}
 
 """
 
 def format_examples(name, examples):
-    out = '.. raw:: html\n\n    <div class="example-table">\n\n'
-    for img, code in examples:
-        out += '.. raw:: html\n\n    <div class="example-row"><div class="example-cell-image">\n\n'
-        if img:
-            out += f'.. image:: /images/reference/{img}\n    :alt: example picture for {name}\n\n'
-        out += '.. raw:: html\n\n    </div><div class="example-cell-code">\n\n'
-        out += f'.. code:: python\n    :number-lines:\n\n{textwrap.indent(code, "    ")}\n\n'
-        out += '.. raw:: html\n\n    </div></div>\n\n'
-    out += '.. raw:: html\n\n    </div>'
+    out = ''
+
+    if examples:
+        out += '\nExamples\n========\n\n'
+        out += '.. raw:: html\n\n    <div class="example-table">\n\n'
+        for img, code in examples:
+            out += '.. raw:: html\n\n    <div class="example-row"><div class="example-cell-image">\n\n'
+            if img:
+                out += f'.. image:: /images/reference/{img}\n    :alt: example picture for {name}\n\n'
+            out += '.. raw:: html\n\n    </div><div class="example-cell-code">\n\n'
+            out += f'.. code:: python\n    :number-lines:\n\n{textwrap.indent(code, "    ")}\n\n'
+            out += '.. raw:: html\n\n    </div></div>\n\n'
+        out += '.. raw:: html\n\n    </div>\n'
 
     return out
 
 
-def parameter_format(var, desc):
-    try:
-        varname, vartype = var.split(':')
-        return f'* **{varname}**: `{vartype.strip()}` - {desc}'
-    except:
-        return f'* **{var}**: - {desc}'
+def format_signatures(signatures):
+    out = ''
+
+    if signatures:
+        out += '\nSyntax\n======\n\n.. code:: python\n\n'
+        out += textwrap.indent('\n'.join(signatures), '    ')
+
+    return out
+
+
+def format_parameters(variables):
+    out = ''
+
+    if variables:
+        out += '\nParameters\n==========\n\n'
+        for var, desc in variables.items():
+            if ':' in var:
+                varname, vartype = var.split(':')
+                out += f'* **{varname}**: `{vartype.strip()}` - {desc}\n'
+            else:
+                out += f'* **{var}**: - {desc}\n'
+        out += '\n'
+
+    return out
 
 
 def write_doc_rst_files():
@@ -81,28 +85,24 @@ def write_doc_rst_files():
     now_nikola = now.strftime('%Y-%m-%d %H:%M:%S %Z%z')[:-2] + ':00'
     now_pretty = now.strftime('%B %d, %Y %H:%M:%S%P %Z')
     for docfile in sorted(PY5_API_EN.glob('*.txt')):
-        stem = docfile.stem.lower()
         doc = Documentation(docfile)
         name = doc.meta['name']
+        stem = docfile.stem.lower()
+
         description = doc.description
         m = FIRST_SENTENCE_REGEX.match(description)
         first_sentence = m.group() if m else description
-        signatures = textwrap.indent('\n'.join(doc.signatures), '    ')
-        parameters = '\n'.join(parameter_format(*x) for x in doc.variables.items())
+
         examples = format_examples(name, doc.examples)
+        signatures = format_signatures(doc.signatures)
+        parameters = format_parameters(doc.variables)
+
+        doc_rst = DOC_TEMPLATE.format(
+            name, stem, now_nikola, first_sentence, examples, description,
+            signatures, parameters, now_pretty)
 
         with open(DEST_DIR / (stem + '.rst'), 'w') as f:
-            f.write(DOC_TEMPLATE.format(
-                name,
-                stem,
-                now_nikola,
-                first_sentence,
-                examples,
-                description,
-                signatures,
-                parameters,
-                now_pretty,
-            ))
+            f.write(doc_rst)
 
 
 if not DEST_DIR.exists():
