9647521b630a98202555a1515839e60b99bbdf3f
Jim <james.schmitz@gmail.com>
2021-04-15 17:08:30 -0400

    py5shape docs

--- py5_docs/scripts/run_example_code.py
+++ py5_docs/scripts/run_example_code.py
@@ -56,7 +56,7 @@ try:
             # skip these for now, but I should probably include them later
             continue
 
-        if doc.meta.get('pclass') != 'PFont':  #
+        if doc.meta.get('pclass') != 'PShape':  #
         # if doc.meta.get('category') != 'typography':
             continue
         # if doc.meta.get('category') != 'image' or doc.meta.get('subcategory') != 'loading_displaying':
