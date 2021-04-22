0bdf33d6bfc0d8bd1e7bcede02bd22725afc5cbb
Jim <james.schmitz@gmail.com>
2021-04-22 10:51:02 -0400

    get texture u v docs

--- py5_docs/scripts/run_example_code.py
+++ py5_docs/scripts/run_example_code.py
@@ -59,7 +59,7 @@ try:
         # if doc.meta.get('pclass') != 'PShape':  #
         # if doc.meta.get('category') != 'typography':
         # if doc.meta.get('category') != 'image' or doc.meta.get('subcategory') != 'loading_displaying':
-        if doc.meta.get('name') not in ['texture()', 'set_texture()', 'set_texture_uv()', 'set_texture_mode()']:
+        if doc.meta.get('pclass') != 'PShape' or doc.meta.get('name') not in ['texture()', 'vertex()', 'get_texture_u()', 'get_texture_v()']:
             continue
 
         for image, code in doc.examples:
