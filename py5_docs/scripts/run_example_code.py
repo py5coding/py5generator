a868064124e8e6c0b15b1da33b2a146722321d59
Jim <james.schmitz@gmail.com>
2021-04-24 22:13:48 -0400

    begin end contour docs

--- py5_docs/scripts/run_example_code.py
+++ py5_docs/scripts/run_example_code.py
@@ -59,7 +59,7 @@ try:
         # if doc.meta.get('pclass') != 'PShape':  #
         # if doc.meta.get('category') != 'typography':
         # if doc.meta.get('category') != 'image' or doc.meta.get('subcategory') != 'loading_displaying':
-        if doc.meta.get('pclass') != 'PShape' or doc.meta.get('name') not in ['set_path()', 'apply_matrix()']:
+        if doc.meta.get('pclass') != 'PShape' or doc.meta.get('name') not in ['begin_contour()', 'end_contour()']:
             continue
 
         for image, code in doc.examples:
