4f90a9500587357aeb5fc72458e25371dbefea44
Jim <james.schmitz@gmail.com>
2021-05-03 16:36:47 -0400

    more improvements to script

--- py5_docs/scripts/generate_py5graphics_docs.py
+++ py5_docs/scripts/generate_py5graphics_docs.py
@@ -19,9 +19,13 @@ relevant_items = list(py5graphics_data.query("type != 'static field' and impleme
 
 website_slugs = [f.stem for f in PY5_REF_DIR.glob("*.rst")]
 
+EXTRA_DESCRIPTION = """
+
+This {0} is the same as :doc:`{1}` but applied to a ``Py5Graphics`` object. To see example code for how it can be used, see :doc:`{1}`.
+"""
+
 
 # TODOs
-# add language that links back to the Sketch doc page (but not for mask) and explains how it is different
 # fix new variable names and signature issues
 
 DESCRIPTION_REPLACEMENTS = {
@@ -41,13 +45,17 @@ for item in relevant_items:
 
     doc.meta['pclass'] = 'PGraphics'
     doc.examples = []
+    item_type = doc.meta['type']
 
     for doclink in REST_DOC_LINK.findall(doc.description):
         test_slug = f"py5graphics_{doclink[6:-1]}"
         if test_slug in website_slugs:
-            doc.description.replace(doclink, f":doc:`{test_slug}`")
+            doc.description = doc.description.replace(doclink, f":doc:`{test_slug}`")
 
     for find_replace in DESCRIPTION_REPLACEMENTS:
         doc.description = doc.description.replace(*find_replace)
+
+    if item != 'mask':
+        doc.description += EXTRA_DESCRIPTION.format(item_type, item)
 
     doc.write(py5graphics_docfile)
