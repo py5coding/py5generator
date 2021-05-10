39204882c1cf19e8097347f8ec2056747d8a574c
Jim <james.schmitz@gmail.com>
2021-05-10 10:47:18 -0400

    example code for py5graphics np_pixels tools

--- py5_docs/scripts/run_example_code.py
+++ py5_docs/scripts/run_example_code.py
@@ -59,7 +59,7 @@ try:
         # if doc.meta.get('pclass') != 'PShape':  #
         # if doc.meta.get('category') != 'typography':
         # if doc.meta.get('category') != 'image' or doc.meta.get('subcategory') != 'loading_displaying':
-        if doc.meta.get('pclass') != 'PShape' or doc.meta.get('name') not in ['set_stroke_cap()', 'set_stroke_join()']:
+        if doc.meta.get('pclass') != 'PGraphics' or doc.meta.get('name') not in ['load_np_pixels()', 'set_np_pixels()', 'update_np_pixels()', 'np_pixels[]']:
             continue
 
         for image, code in doc.examples:
