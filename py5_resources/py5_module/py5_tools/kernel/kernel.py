e0752c39b3d7869926fa9be558fa2ac9c372e8c6
Jim <james.schmitz@gmail.com>
2021-05-23 11:37:34 -0400

    bump version to 0.4a1

--- py5_resources/py5_module/py5_tools/kernel/kernel.py
+++ py5_resources/py5_module/py5_tools/kernel/kernel.py
@@ -72,7 +72,7 @@ class Py5Kernel(IPythonKernel):
                        *_PY5_HELP_LINKS]).tag(config=True)
 
     implementation = 'py5'
-    implementation_version = '0.4a1.dev0'
+    implementation_version = '0.4a1'
 
 
 class Py5App(IPKernelApp):
