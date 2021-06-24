8329ea45cec932fb46f7305b399bb440c07cfad9
Jim <james.schmitz@gmail.com>
2021-06-24 16:06:24 -0400

    init commit of py5bot

---
+++ py5_resources/py5_module/py5_tools/py5bot/kernel.py
@@ -0,0 +1,66 @@
+# *****************************************************************************
+#
+#   Part of the py5 library
+#   Copyright (C) 2020-2021 Jim Schmitz
+#
+#   This library is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 2.1 of the License, or (at
+#   your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this library. If not, see <https://www.gnu.org/licenses/>.
+#
+# *****************************************************************************
+from IPython.core.interactiveshell import InteractiveShellABC
+from ipykernel.kernelapp import IPKernelApp
+
+from traitlets import Type, Instance, Unicode, List
+
+from ..kernel.kernel import Py5Shell, Py5Kernel
+from . import py5bot
+
+first_run = True
+
+
+class Py5BotShell(Py5Shell):
+
+    banner2 = Unicode("Activating py5bot").tag(config=True)
+
+    def run_cell(self, raw_cell, store_history=False, silent=False, shell_futures=True):
+        if not raw_cell.strip().startswith('# *** PY5BOT_CODE_INIT ***'):
+            # TODO: this should use split_setup code
+            py5bot_settings = raw_cell.split('\n', maxsplit=1)[0]
+            py5bot_setup = '\n'.join(raw_cell.splitlines()[1:])
+            py5bot.write_code(py5bot_settings, py5bot_setup)
+            raw_cell = py5bot.PY5BOT_CODE
+
+        return super(Py5BotShell, self).run_cell(
+            raw_cell, store_history=store_history, silent=silent, shell_futures=shell_futures)
+
+InteractiveShellABC.register(Py5BotShell)
+
+
+class Py5BotKernel(Py5Kernel):
+    shell = Instance('IPython.core.interactiveshell.InteractiveShellABC',
+                     allow_none=True)
+    shell_class = Type(Py5BotShell)
+
+    implementation = 'py5bot'
+    implementation_version = '0.4a3.dev0'
+
+
+class Py5BotApp(IPKernelApp):
+    name = 'py5bot-kernel'
+
+    kernel_class = Type('py5_tools.py5bot.Py5BotKernel',
+                        klass='ipykernel.kernelbase.Kernel').tag(config=True)
+
+    exec_lines = List(Unicode(), [
+        py5bot.PY5BOT_CODE_INIT
+    ]).tag(config=True)
---
+++ py5_resources/py5_module/py5_tools/py5bot/py5bot.py
@@ -0,0 +1,88 @@
+# *****************************************************************************
+#
+#   Part of the py5 library
+#   Copyright (C) 2020-2021 Jim Schmitz
+#
+#   This library is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 2.1 of the License, or (at
+#   your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this library. If not, see <https://www.gnu.org/licenses/>.
+#
+# *****************************************************************************
+
+# TODO: don't pop open a window for JAVA2D renderer
+# TODO: new py5bot logo icon
+# TODO: use code in parsing to check for assignments to reserved words
+# TODO: error messages
+# TODO: use split_setup in py5bot shell
+
+
+_PY5BOT_SETTINGS_FILENAME_ = '/tmp/py5bot_settings_code.py'
+_PY5BOT_SETUP_FILENAME_ = '/tmp/py5bot_setup_code.py'
+
+PY5BOT_CODE_INIT = """
+# *** PY5BOT_CODE_INIT ***
+
+import time as _time
+import ast as _ast
+
+from PIL import Image as _Image
+
+import py5_tools
+py5_tools.set_imported_mode(True)
+from py5 import *
+
+_PY5BOT_SETTINGS_FILENAME_ = '{0}'
+_PY5BOT_SETUP_FILENAME_ = '{1}'
+
+
+def settings():
+    settings_ast = _ast.parse(_PY5BOT_SETTINGS_, filename=_PY5BOT_SETTINGS_FILENAME_, mode='exec')
+    exec(compile(py5_tools.parsing.transform_py5_code(settings_ast), filename=_PY5BOT_SETTINGS_FILENAME_, mode='exec'))
+
+
+def setup():
+    global _PY5_OUTPUT_
+    _PY5_OUTPUT_ = None
+
+    setup_ast = _ast.parse(_PY5BOT_SETUP_, filename=_PY5BOT_SETUP_FILENAME_, mode='exec')
+    exec(compile(py5_tools.parsing.transform_py5_code(setup_ast), filename=_PY5BOT_SETUP_FILENAME_, mode='exec'))
+
+    load_np_pixels()
+    arr = np_pixels()[:, :, 1:]
+    _PY5_OUTPUT_ = _Image.fromarray(arr)
+    exit_sketch()
+""".format(_PY5BOT_SETTINGS_FILENAME_, _PY5BOT_SETUP_FILENAME_)
+
+
+PY5BOT_CODE = """
+with open(_PY5BOT_SETTINGS_FILENAME_, 'r') as f:
+    _PY5BOT_SETTINGS_ = f.read()
+
+with open(_PY5BOT_SETUP_FILENAME_, 'r') as f:
+    _PY5BOT_SETUP_ = f.read()
+
+run_sketch()
+while not is_dead:
+    _time.sleep(0.05)
+if is_dead_from_error:
+    exit_sketch()
+
+_PY5_OUTPUT_
+"""
+
+
+def write_code(settings_code, setup_code):
+    with open(_PY5BOT_SETTINGS_FILENAME_, 'w') as f:
+        f.write(settings_code)
+
+    with open(_PY5BOT_SETUP_FILENAME_, 'w') as f:
+        f.write(setup_code)
