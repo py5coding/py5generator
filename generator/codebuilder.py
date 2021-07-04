f61ba37f3c5decfe5132ab50686b7cd07fae7bf5
Jim <james.schmitz@gmail.com>
2021-07-04 09:55:09 -0400

    new end methods comment marker for mixins

--- generator/codebuilder.py
+++ generator/codebuilder.py
@@ -266,6 +266,8 @@ class CodeBuilder:
         overloaded = set()
         self.module_members.append(f'\n{"#" * 78}\n# module functions from {filename.name}\n{"#" * 78}\n')
         method_code = code.split('*** BEGIN METHODS ***')[1].strip()
+        if end_methods_indx := method_code.find('*** END METHODS ***') >= 0:
+            method_code = method_code[:end_methods_indx].strip()
         for decorator, fname, arg0, args, rettypestr, fake_decorator in METHOD_REGEX.findall(method_code):
             if fname.startswith('_') and not fake_decorator:
                 continue
