b1f56ddecb06b7d76ca42f561076c6a6ca7195de
Jim <james.schmitz@gmail.com>
2021-08-11 10:55:22 -0400

    simplify code

--- py5_resources/py5_module/py5/mixins/pixels.py
+++ py5_resources/py5_module/py5/mixins/pixels.py
@@ -50,13 +50,10 @@ class PixelArray:
         if (newval := _hex_converter(val)) is not None:
             val = newval
 
-        if isinstance(val, (int, np.integer)):
-            try:
-                self.instance.pixels[index] = val
-            except:
-                raise RuntimeError('Cannot parse "' + str(val) + '" as a color') from None
-        else:
-            raise RuntimeError('Cannot parse "' + str(val) + '" as a color')
+        try:
+            self.instance.pixels[index] = val
+        except:
+            raise RuntimeError('Cannot parse "' + str(val) + '" as a color') from None
 
 
 class PixelMixin:
