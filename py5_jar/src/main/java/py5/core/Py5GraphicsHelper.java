ba79f27d8786d33900cd759681abd7b8eae6f948
Jim <james.schmitz@gmail.com>
2022-06-04 08:08:04 -0400

    address #86

--- py5_jar/src/main/java/py5/core/Py5GraphicsHelper.java
+++ py5_jar/src/main/java/py5/core/Py5GraphicsHelper.java
@@ -24,32 +24,37 @@ import processing.core.PGraphics;
 public class Py5GraphicsHelper {
 
   public static void points(PGraphics g, float[][] coordinates) {
+    g.beginShape(PGraphics.POINTS);
     if (coordinates[0].length == 2) {
       for (int i = 0; i < coordinates.length; ++i) {
-        g.point(coordinates[i][0], coordinates[i][1]);
+        g.vertex(coordinates[i][0], coordinates[i][1]);
       }
     } else if (coordinates[0].length == 3) {
       for (int i = 0; i < coordinates.length; ++i) {
-        g.point(coordinates[i][0], coordinates[i][1], coordinates[i][2]);
+        g.vertex(coordinates[i][0], coordinates[i][1], coordinates[i][2]);
       }
     } else {
       throw new RuntimeException("the second axis of parameter coordinates must have a length equal to 2 or 3");
     }
+    g.endShape();
   }
 
   public static void lines(PGraphics g, float[][] coordinates) {
+    g.beginShape(PGraphics.LINES);
     if (coordinates[0].length == 4) {
       for (int i = 0; i < coordinates.length; ++i) {
-        g.line(coordinates[i][0], coordinates[i][1], coordinates[i][2], coordinates[i][3]);
+        g.vertex(coordinates[i][0], coordinates[i][1]);
+        g.vertex(coordinates[i][2], coordinates[i][3]);
       }
     } else if (coordinates[0].length == 6) {
       for (int i = 0; i < coordinates.length; ++i) {
-        g.line(coordinates[i][0], coordinates[i][1], coordinates[i][2], coordinates[i][3], coordinates[i][4],
-            coordinates[i][5]);
+        g.vertex(coordinates[i][0], coordinates[i][1], coordinates[i][2]);
+        g.vertex(coordinates[i][3], coordinates[i][4], coordinates[i][5]);
       }
     } else {
       throw new RuntimeException("the second axis of parameter coordinates must have a length equal to 4 or 6");
     }
+    g.endShape();
   }
 
   public static void vertices(PGraphics g, float[][] coordinates) {
