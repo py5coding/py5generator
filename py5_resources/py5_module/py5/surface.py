fd754bc8cf0ebf4ef24daeea2e2f5d08b769b0eb
Jim <james.schmitz@gmail.com>
2022-07-02 09:38:00 -0400

    remove dead code that should have been removed over a year ago

--- py5_resources/py5_module/py5/surface.py
+++ py5_resources/py5_module/py5/surface.py
@@ -30,7 +30,7 @@ py5surface_class_members_code = None  # DELETE
 def _return_py5surface(f):
     @functools.wraps(f)
     def decorated(self_, *args):
-        return Py5Surface(f(self_, *args), getattr(self_, '_pimage_cache', None))
+        return Py5Surface(f(self_, *args))
     return decorated
 
 
@@ -38,9 +38,8 @@ class Py5Surface:
     """$classdoc_Py5Surface
     """
 
-    def __init__(self, psurface, pimage_cache):
+    def __init__(self, psurface):
         self._instance = psurface
-        self._pimage_cache = pimage_cache
 
 
 {py5surface_class_members_code}
