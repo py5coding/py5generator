fc7f780fe2cc74c2c996e76cd26953647661378f
Jim <james.schmitz@gmail.com>
2022-12-27 10:11:56 -0500

    clean up todos

--- py5_resources/py5_module/py5_tools/hooks/zmq_hooks.py
+++ py5_resources/py5_module/py5_tools/hooks/zmq_hooks.py
@@ -26,7 +26,6 @@ import PIL
 try:
     from py5jupyter.widgets import Py5SketchPortal
 except ImportError:
-    # TODO: deprecated
     class Py5SketchPortal(widgets.Image):
         def __init__(self, sketch, w, h):
             super().__init__()
@@ -49,7 +48,6 @@ def sketch_portal(*, time_limit: float = 0.0, throttle_frame_rate: float = 30,
     if not environment.in_jupyter_zmq_shell:
         raise RuntimeError('The sketch_widget() function can only be used with ZMQInteractiveShell (such as Jupyter Lab)')
     if issubclass(Py5SketchPortal, widgets.Image):
-        # TODO: deprecated
         warnings.warn('Please install the py5jupyter package for interactive Py5SketchPortal functionality.')
 
     if sketch is None:
