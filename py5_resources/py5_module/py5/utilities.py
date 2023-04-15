2342a61bdd5ed3aeb2df0fa4609fcdd2f58d6d91
Jim <james.schmitz@gmail.com>
2023-04-14 22:34:02 -0400

    don't throw error if py5utilities not available

--- py5_resources/py5_module/py5/utilities.py
+++ py5_resources/py5_module/py5/utilities.py
@@ -27,13 +27,21 @@ class Py5Utilities:
     """
     def __init__(self, sketch):
         self._sketch = sketch
-        self._instance = jpype.JClass('py5utils.Py5Utilities')(sketch._instance)
-        self._dir = list(set(dir(self._instance)) - set('equals getClass hashCode notify notifyAll wait toString'.split()))
+        try:
+            self._instance = jpype.JClass('py5utils.Py5Utilities')(sketch._instance)
+            self._dir = list(set(dir(self._instance)) - set('equals getClass hashCode notify notifyAll wait toString'.split()))
+        except:
+            self._instance = None
+            self._dir = []
+
         from .java_conversion import convert_to_python_type
         self._convert_to_python_type = convert_to_python_type
 
     def __str__(self) -> str:
-        return "Py5Utilities(sketch=" + self._sketch.__str__() + ")"
+        if self._instance is None:
+            return "Py5Utilities(<this feature is not available for this Sketch>)"
+        else:
+            return "Py5Utilities(sketch=" + self._sketch.__str__() + ")"
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -42,7 +50,9 @@ class Py5Utilities:
         return self._dir
 
     def __getattr__(self, name):
-        if hasattr(self._instance, name):
+        if self._instance is None:
+            raise AttributeError("Py5Utilities class was not instantiated for this Sketch. Check your classpath if you believe this to be an error.")
+        elif hasattr(self._instance, name):
             attr = getattr(self._instance, name)
             if callable(attr):
                 return lambda *args: self._convert_to_python_type(attr(*args))
