235365f0abdc49c89241ea1b7f8260931dc8e732
Jim <james.schmitz@gmail.com>
2023-06-18 21:20:58 -0400

    improve error message

--- py5_resources/py5_module/py5_tools/import_hook.py
+++ py5_resources/py5_module/py5_tools/import_hook.py
@@ -104,7 +104,7 @@ class Py5ImportedModeLoader(Loader):
         code_ast = ast.parse(code_src, filename=self.filename, mode='exec')
         problems = py5_tools.parsing.check_reserved_words(code_src, code_ast)
         if problems:
-            msg = 'There ' + ('is a problem' if len(problems) == 1 else f'are {len(problems)} problems') + ' with the code in ' + str(self.filename) + '.\n'
+            msg = 'There ' + ('is a problem' if len(problems) == 1 else f'are {len(problems)} problems') + ' with the code import ' + str(module.__name__) + '.\n'
             msg += '=' * len(msg) + '\n' + '\n'.join(problems)
             raise ImportError(msg)
 
