8fda279efd5036de3ee272961bcabaadaa5abc14
Jim <james.schmitz@gmail.com>
2024-07-29 07:13:59 -0400

    bump version to 0.10.2a0

--- py5_resources/py5_module/py5/__init__.py
+++ py5_resources/py5_module/py5/__init__.py
@@ -115,7 +115,7 @@ except ImportError:
     pass
 
 
-__version__ = "0.10.2.dev4"
+__version__ = "0.10.2a0"
 
 _PY5_USE_IMPORTED_MODE = py5_tools.get_imported_mode()
 py5_tools._lock_imported_mode()
