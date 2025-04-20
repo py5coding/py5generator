32bd086402635e8e3423d6c1b9044a37c2a62207
Jim <james.schmitz@gmail.com>
2025-04-19 23:29:26 -0400

    fix ant config so build can also run on macos

--- py5_jar/src/main/java/py5/core/SketchBase.java
+++ py5_jar/src/main/java/py5/core/SketchBase.java
@@ -66,7 +66,13 @@ public class SketchBase extends PApplet {
       System.setProperty("jogamp.gluegen.UseTempJarCache", "false");
     }
 
-    String variant = PConstants.platformNames[PApplet.platform] + "-" + System.getProperty("os.arch");
+    String variant = PConstants.platformNames[PApplet.platform];
+    if (variant.equals("macos")) {
+      variant = "macosx-universal";
+    } else {
+      // TODO: may need to do something special for Raspberry Pi computers
+      variant += "-" + System.getProperty("os.arch");
+    }
     String joglPath = py5Path + File.separator + "natives" + File.separator + variant;
     String javaLibraryPath = System.getProperty("java.library.path");
     if (javaLibraryPath == null) {
