0ae4b0acaac52a58e0bd4a91eafb41831c696b2b
Jim <james.schmitz@gmail.com>
2026-01-03 00:54:55 -0500

    fix #734

--- py5_resources/py5_module/py5_tools/jvm.py
+++ py5_resources/py5_module/py5_tools/jvm.py
@@ -166,7 +166,10 @@ def _start_jvm() -> None:
     if "--enable-native-access=ALL-UNNAMED" not in _options:
         _options.append("--enable-native-access=ALL-UNNAMED")
 
-    if "--enable-native-access=javafx.graphics" not in _options:
+    if (
+        any([f.name == "javafx.graphics.jar" for f in _classpath])
+        and "--enable-native-access=javafx.graphics" not in _options
+    ):
         _options.append("--enable-native-access=javafx.graphics")
 
     jpype.startJVM(default_jvm_path, *_options, convertStrings=False)
