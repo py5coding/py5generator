ecba54af300451aeb1660e58af25a44f0adf47a3
Jim <james.schmitz@gmail.com>
2026-01-09 04:38:18 -0500

    bump version to 0.10.10.dev0

--- py5_resources/py5_module/py5_tools/constants.py
+++ py5_resources/py5_module/py5_tools/constants.py
@@ -21,7 +21,7 @@ import os
 import platform
 from pathlib import Path
 
-VERSION = "0.10.9a1"
+VERSION = "0.10.10.dev0"
 PROCESSING_BUILD_NUMBER = 1310
 
 if not (PY5_HOME := os.environ.get("PY5_HOME")):
