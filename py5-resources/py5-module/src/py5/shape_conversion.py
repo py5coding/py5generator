876754497d34ff4ec336d2001d1e8bec48e4e395
Jim Schmitz <james.schmitz@gmail.com>
2026-03-11 16:27:08 -0400

    add flip_v param

--- py5-resources/py5-module/src/py5/shape_conversion.py
+++ py5-resources/py5-module/src/py5/shape_conversion.py
@@ -470,7 +470,8 @@ try:
         if isinstance(obj.visual, TextureVisuals):
             use_texture = True
             uv = obj.visual.uv[obj_faces_ravel]
-            uv[:, 1] = 1 - uv[:, 1]
+            if kwargs.get("flip_v", False):
+                uv[:, 1] = 1 - uv[:, 1]
             vertices = np.hstack([vertices, uv])
 
             if "texture" in kwargs:
