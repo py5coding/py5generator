db929c46225b86fab571aab5b182399134f3a3e5
Jim Schmitz <james.schmitz@gmail.com>
2026-07-01 10:52:34 -0400

    bump version to 0.10.11a0

--- py5-resources/py5-module/src/py5_tools/constants.py
+++ py5-resources/py5-module/src/py5_tools/constants.py
@@ -21,8 +21,8 @@ import os
 import platform
 from pathlib import Path
 
-VERSION = "0.10.11.dev1"
-PROCESSING_BUILD_NUMBER = 1313
+VERSION = "0.10.11a0"
+PROCESSING_BUILD_NUMBER = 1433
 
 if not (PY5_HOME := os.environ.get("PY5_HOME")):
     if platform.system() == "Windows":
